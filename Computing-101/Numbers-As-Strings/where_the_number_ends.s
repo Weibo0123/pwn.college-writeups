375218426e0f62491cb83ed34df087702eb676b0
Weibo0123 <weibolin322@gmail.com>
2026-08-04 16:55:04 -0500

    Added the writeup for Where the Number Ends

---
+++ Computing-101/Numbers-As-Strings/where_the_number_ends.s
@@ -0,0 +1,40 @@
+.intel_syntax noprefix
+
+.global atoi
+
+atoi:
+    mov rdx, 0
+    mov r8, 0
+    movzx rsi, byte ptr [rdi]
+    cmp rsi, 0x2d
+    je negative
+    loop:
+        movzx rsi, byte ptr [rdi]
+        sub rsi, 0x30
+        cmp rsi, 9
+        ja done
+        imul rdx, 10
+        add rdx, rsi
+        add rdi, 1
+        jmp loop
+
+negative:
+    mov r8, 1
+    add rdi, 1
+    jmp loop
+
+add_negative:
+    neg rdx
+    mov rax, rdx
+    ret
+
+done:
+    cmp r8, 1
+    je add_negative
+    mov rax, rdx
+    ret
+
+
+
+
+
